add00a0177277b5d75299ff4df1ee4dcb76f62d4
KonstantinosGkolias <it14158@uom.edu.gr>
2016-10-07 20:38:21 +0300

    initial commit

--- app/src/main/java/mobile/uom/gr/lesson1hello/MainActivity.java
+++ app/src/main/java/mobile/uom/gr/lesson1hello/MainActivity.java
@@ -1,7 +1,9 @@
 package mobile.uom.gr.lesson1hello;
 
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void foo(View view){
+        System.out.println("Button clicked");
     }
 }
